242b06707e33ef209d4ea1ba8f516894fdecd61e
Pacome Simon Mbonimpa <smbonimpa2011@gmail.com>
2022-03-18 01:37:27 +0200

    home and about pages updates

--- js/indexAbout.js
+++ js/indexAbout.js
@@ -2,16 +2,28 @@ var myToggler = false;
 document.getElementById("menuicon").onclick = function() {myFunction()};
 
 function myFunction() {
-    var menuVisibility = ""
+    var menuVisibility = "";
+    var brandnameColor = "";
+    var navbarColor = "";
+    var menuiconColor =""
     if(myToggler){
         myToggler = false;
-        menuVisibility = "none"
+        menuVisibility = "none";
+        brandnameColor = "black";
+        navbarColor = "white";
+        menuiconColor = "rgba(0, 0, 255, 0.49)";
     }
     else{
         myToggler = true;
         menuVisibility = "grid";
+        brandnameColor = "white";
+        navbarColor = "rgba(128, 128, 255, 0.95)";
+        menuiconColor = "white";
     }
   document.getElementById("navbar-expanded").style.display = menuVisibility;
+  document.getElementById("brandname").style.color = brandnameColor;
+  document.getElementById("navbar2").style["background"] = navbarColor;
+  document.getElementById("menuicon").style.color = menuiconColor;
 }
 
 
