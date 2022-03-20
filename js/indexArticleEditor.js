2d4ba9fe88849c12519fbc7c1dc68ac2285eb293
Pacome Simon Mbonimpa <smbonimpa2011@gmail.com>
2022-03-20 16:52:09 +0200

    basic layout of blog and admin pages

---
+++ js/indexArticleEditor.js
@@ -0,0 +1,38 @@
+var myToggler = false;
+document.getElementById("menuicon").onclick = function() {myFunction()};
+
+function myFunction() {
+    var menuVisibility = "";
+    var brandnameColor = "";
+    var navbarColor = "";
+    var menuiconColor =""
+    if(myToggler){
+        myToggler = false;
+        menuVisibility = "none";
+        brandnameColor = "black";
+        navbarColor = "white";
+        menuiconColor = "rgba(0, 0, 255, 0.49)";
+    }
+    else{
+        myToggler = true;
+        menuVisibility = "grid";
+        brandnameColor = "white";
+        navbarColor = "rgba(128, 128, 255, 0.95)";
+        menuiconColor = "white";
+    }
+  document.getElementById("navbar-expanded").style.display = menuVisibility;
+  document.getElementById("brandname").style.color = brandnameColor;
+  document.getElementById("navbar2").style["background"] = navbarColor;
+  document.getElementById("menuicon").style.color = menuiconColor;
+}
+
+
+function myFunc(x) {
+    if (!x.matches) {
+        document.getElementById("navbar-expanded").style.display = "none";
+    }
+  }
+  
+  var x = window.matchMedia("(max-width: 890px)")
+  myFunc(x) 
+  x.addListener(myFunc)
